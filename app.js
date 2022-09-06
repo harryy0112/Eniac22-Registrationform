26b6cbed04dac4e498cc2edeaba616e62950cd07
Hardik Agarwal <agarwalhardik562@gmail.com>
2022-09-07 01:56:26 +0530

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const valName = new RegExp("^[a-zA-Z ]{2,30}$");
 const valEmail = new RegExp("^[a-zA-Z]*20[0-9]{5}@akgec\.ac\.in$");
-const errCode = ["*Max. 25, Characters Only", "*Use College Email ID"];
+const errorCode = ["*Max. 25, Characters Only", "*Use College Email ID"];
+
+function validateForm() {
+
+  clearErrors();
+  let returnVal = true;
+  const nameError = document.getElementById("name");
+  const emailError = document.getElementById("email");
+  const Name = form['Name'].value;
+  const Email = form['Email'].value;
+
+  if (!valName.test(Name)) {
+    seterror(nameError[1], errorCode[0]);
+    returnVal = false;
+  }
+
+  if (!valEmail.test(Email)) {
+    seterror(emailError[1], errorCode[1]);
+    returnVal = false;
+  }
+  return returnVal;
+}
+
+function seterror(id, error) {
+  element = document.getElementById(id);
+  element.getElementsByClassName('formerror')[0].innerHTML = error;
+}
+
+function clearErrors() {
+  errors = document.getElementsByClassName('formerror');
+  for (let item of errors) {
+    item.innerHTML = "";
+  }
+}
 
 const inputs = document.querySelectorAll(".input-field");
 
@@ -18,39 +51,4 @@ function moveSlider() {
   let index = this.dataset.value;
   const textSlider = document.querySelector(".text-group");
   textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
-}
-
-function validateForm() {
-  resetErr();
-
-  let returnVal = true;
-
-  const nameErr = document.getElementById("name")
-  const emailErr = document.getElementById("email");
-
-  const Name = form['Name'].value;
-  const Email = form['Email'].value;
-
-  if (!valName.test(Name)) {
-    throwErr(nameErr[1], errCode[0]);
-    returnVal = false;
-  }
-
-  if (!valEmail.test(Email)) {
-    throwErr(emailErr[1], errCode[1]);
-    returnVal = false;
-  }
-
-  return returnVal;
-}
-
-function throwErr(id, err) {
-  id.innerHTML = err;
-}
-
-function resetErr() {
-  const errors = document.getElementsByClassName("err");
-  for (let error of errors) {
-    error.innerHTML = "";
-  }
 }
